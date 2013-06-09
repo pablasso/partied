e31bd0f7615a7358a33245d79339802246cf051f
Juan Pablo Ortiz <pablasso@gmail.com>
2013-06-09 16:40:37 -0500

    Zooming and Panning on the photo details.

--- Partied/PhotoViewController.m
+++ Partied/PhotoViewController.m
@@ -2,9 +2,10 @@
 #import <UIImageView+AFNetworking.h>
 #import "Photo.h"
 
-@interface PhotoViewController ()
+@interface PhotoViewController () <UIScrollViewDelegate>
 
 @property (nonatomic, strong) Photo *photo;
+@property (nonatomic, strong) IBOutlet UIScrollView *scrollView;
 @property (nonatomic, strong) IBOutlet UIImageView *imageView;
 
 @end
@@ -21,8 +22,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.navigationController.navigationBar.backItem.title = @"Back";
     [self.imageView setImageWithURL:[NSURL URLWithString:self.photo.photoURL]];
+    self.scrollView.contentSize = self.imageView.bounds.size;
+    self.scrollView.minimumZoomScale = 1.0f;
+    self.scrollView.maximumZoomScale = 5.0f;
+    self.scrollView.bounces = NO;
+}
+
+#pragma mark - UIScrollViewDelegate
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    return self.imageView;
 }
 
 @end
